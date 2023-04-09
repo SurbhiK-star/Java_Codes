eda91307f328c54b85a60226ce5fcaf23c8a6d2c
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-09 12:06:40 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,13 @@
+class Loops
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        do{
+            System.out.println("Hello");
+            i=i+1;
+        }
+        while(i<=5);
+        System.out.println("i="+i);
+    }
+}
