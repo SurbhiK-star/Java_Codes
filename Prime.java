ba8a51a40b32da4a74cf6ad34989a4a2a342e1dc
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-09 11:59:24 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,20 @@
+ class Prime {
+    public static void main(String args[])
+    {
+        int num=4;boolean flag=true;
+        for(int i=2;i<=num/2;i++)
+        {
+            if(num%i==0)
+            {
+                flag=false;break;
+            }
+        }
+        if(flag==true)
+        {
+            System.out.println("Number is Prime");
+        }
+        else{
+            System.out.println("Number is Not prime");
+        }
+    }
+}
