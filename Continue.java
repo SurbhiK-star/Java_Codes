a490ee1025728627474d13f10b369f0cd1c7cd00
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-11 16:35:07 +0530

    Add files via upload

---
+++ Continue.java
@@ -0,0 +1,12 @@
+class Continue                            //skip krta hain ye 
+{
+    public static void main(String args[]){
+        for(inti=1;i<=5;i++)
+        {
+            System.out.println("Hello");
+            if(i==3){
+                continue;
+            }
+        }
+    }
+}
