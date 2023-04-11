6f1f0f2897755541104fb62e539869ad4abdd09a
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-11 16:55:37 +0530

    Add files via upload

---
+++ Nine.java
@@ -0,0 +1,16 @@
+class Nine
+{
+public static void main(String args[])
+{
+int CET=20;
+int Dip=40;
+if(CET>=40)
+{
+	System.out.println("Eligible by CET");
+}
+ if(Dip>=40)
+{
+	System.out.println("Eligible by Dip");
+}
+}
+}
