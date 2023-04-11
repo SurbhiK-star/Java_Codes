e20226148e8135d32500148dc7f6c3e42d029c5e
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-11 15:38:24 +0530

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,10 @@
+class Three
+{
+    public static void main(String args[])
+    {
+        long a=100000;
+        int b = (int)a;
+        System.out.println("Value of a = " + a);
+        System.out.println("Value of b = " +b);
+    }
+}
