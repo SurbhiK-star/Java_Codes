a6ee829afa6d1d308ebfc27cee753a5f9f751aa0
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-11 16:51:10 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,24 @@
+class Marks
+{
+    public static void main(String args[])
+    {
+        int marks=10;
+        switch(marks)
+        {
+            case 10,9,8:
+            System.out.println("Perfect");break;
+
+            case 7,6,5:
+            System.out.println("Average");break;
+
+            case 4,3,2,1:
+            System.out.println("Poor");break;
+
+
+        default:
+           System.out.println("Invalid");
+
+
+        }
+    }
+}
