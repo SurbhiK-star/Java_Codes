5c35d7de9962daf25661556fde73f1e7a3d668d7
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-11 16:57:59 +0530

    Add files via upload

---
+++ Ten.java
@@ -0,0 +1,20 @@
+class Ten
+{
+    public static void main(String args[])
+    {
+        int ssc=50;
+        int hsc=10;
+        if(ssc>=40)
+        {
+            if(hsc>=40)
+            {
+                System.out.println("Eligible for Admission");            }
+        }
+        else{
+                System.out.println("Not Eligible for Admission");
+        }
+    }
+     else{
+            System.out.println("Not Eligible for Admission");
+    }
+}
