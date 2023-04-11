ff7012de6d81df380777831ed662ea528f2c1383
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-11 16:53:48 +0530

    Add files via upload

---
+++ Markstwo.java
@@ -0,0 +1,30 @@
+class Marks
+{
+    public static void main(String args[])
+    {
+        int marks=10;
+        switch(marks)
+        {
+            case 10:
+            case 9:
+            case 8:
+            System.out.println("Perfect");break;
+
+            case 7:
+            case 6:
+            case 5:
+            System.out.println("Average");break;
+
+            case 4:
+            case 3:
+            case 2:
+            case 1:
+            System.out.println("Poor");break;
+
+            default:
+            System.out.println("Invalid");
+
+
+        }
+    }
+}
