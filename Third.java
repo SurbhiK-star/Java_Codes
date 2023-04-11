2afe5663591fe85f94368919434f1d2b650e0243
SurbhiKadale <83442870+SurbhiK-star@users.noreply.github.com>
2023-04-11 15:48:22 +0530

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,9 @@
+class Third {
+    public static void main(String args[])
+    {
+        int q=13;
+        System.out.println("value of third number is=");
+        System.out.println(q);
+        System.out.print("which is a small value");
+    }
+}
